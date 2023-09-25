0e5ca9bf5b123a798b41a9c99bcf23cbe6ac3f32
Gabriel Formigoni <bielformigoni@gmail.com>
2023-09-25 16:50:58 -0300

    fixes

--- components/navigation/item.tsx
+++ components/navigation/item.tsx
@@ -32,14 +32,14 @@ const NavigationItem: React.FC<NavigationItemProps> = ({
           <div
             className={cn(
               "absolute left-0 w-[4px] bg-primary rounded-r-full transition-all",
-              params.serverId !== id && "group-hover:h-[20px]",
-              params.serverId === id ? "h-[36px]" : "h-[8px]"
+              params?.serverId !== id && "group-hover:h-[20px]",
+              params?.serverId === id ? "h-[36px]" : "h-[8px]"
             )}
           />
           <div
             className={cn(
               "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-              params.serverId === id &&
+              params?.serverId === id &&
                 "bg-primary/10 text-primary rounded-[16px]"
             )}
           >
